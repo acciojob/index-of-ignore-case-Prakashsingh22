574aa26fae0406b0d3bed3776de491f9c13570b2
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-26 12:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+	
+	// edge case when string s1 s2 empty 
+	if(s1.length==="")return -1;
+	if(s2.length==="")return 0;
 
-// Please do not change the code below
+	const lowers1 = s1.toLowerCase();
+	const lowers2 = s2.toLowerCase();
+    return lowers1.indexOf(lowers2);
+}
+Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
+	
